25efbfdc56af972a6aa4759846bbe4ff52f8bd07
kevin <kevin@todos.co.kr>
2019-04-30 17:16:55 +0900

    api cors test

--- team/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.utils import timezone
-
-
-class List(models.Model):
-	name = models.CharField(max_length=50)
-	career = models.TextField()
-	creDt = models.DateTimeField(default=timezone.now)
-
--- team/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path,include
-from team import views
-
-
-urlpatterns = [
-    path('', views.list),
-]
