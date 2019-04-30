8ff62c401075a01d3beaca2c3c8417b8b0565528
kevin <kevin@todos.co.kr>
2019-04-30 17:18:03 +0900

    api cors test

---
+++ team/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.utils import timezone
+
+
+class List(models.Model):
+	name = models.CharField(max_length=50)
+	career = models.TextField()
+	creDt = models.DateTimeField(default=timezone.now)
+
